b9ca477024b2f10a0fe3c838297ee832dd9e1065
Aiden-S-Codeler <aiden.sallee@ucas-edu.net>
2025-08-20 10:47:31 -0600

    Hello World

---
+++ practices/helloworld.py
@@ -0,0 +1,8 @@
+# AS 2nd Hello World
+Greetings = input("what is your name? (Please Capitalize)")
+if Greetings == "World":
+    print("Hello World, Im glad your name was world so I could say that!!")
+elif Greetings == "Aiden" or Greetings == "Ms. LaRose":
+    print("Hello admin user", Greetings)
+else:
+    print("Hello", Greetings)
