c62b93b347f466b6924e9fcb77d49d54c594d313
Aiden-S-Codeler <aiden.sallee@ucas-edu.net>
2025-08-28 10:39:14 -0600

    Grade Checker

---
+++ actual_work/grade.py
@@ -0,0 +1,13 @@
+# Grade Maker
+
+grade_eng = int(input("What is your grade percent in English?"))
+grade_CS14000 = int(input("What is your grade percent in Computer Sciences?"))
+grade_worldciv = int(input("What is your grade percent in World Civilization?"))
+grade_adv = int(input("What is your grade percent in Advisory?"))
+grade_math = int(input("What is your grade percent in Math?"))
+grade_art = int(input("What is your grade percent in? Art"))
+grade_biology = int(input("What is your grade percent in Biology?"))
+
+grade_avg = (grade_adv + grade_art + grade_biology + grade_eng + grade_CS14000 + grade_math + grade_worldciv)/7
+
+print("your average grade is",  grade_avg)
