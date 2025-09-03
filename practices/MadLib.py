55e096a894404f79beb5daf02377ae314f93699d
Aiden-S-Codeler <aiden.sallee@ucas-edu.net>
2025-09-03 10:38:04 -0600

    I libbed madly

---
+++ practices/MadLib.py
@@ -0,0 +1,7 @@
+# AS 2nd Mad Lib
+Adjective1 = input("Give me an Adjective")
+Noun1 = input("Give me a noun")
+Adjective2 = input("Give me an Adjective")
+Noun2 = input("Give me a noun")
+Place = input("Give me a place")
+print("I would like a " + Adjective1 + " " + Noun1 + ". Because of this I went to the " + Adjective2 + " " + Noun2 + ". Due to the fact that the " + Adjective2 + " " + Noun2 + " lives in " + Place + ", it has magical properties and was able to give me what I came for. I left " + Place + ", and went home to use the " + Adjective1 + " " + Noun1 + " just as intended. I ate it all in one " + Adjective1 + " bite.")
