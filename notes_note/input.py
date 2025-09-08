024835313a35d73fa0bcac6c41cb6c7f04fe0100
Aiden-S-Codeler <aiden.sallee@ucas-edu.net>
2025-09-08 10:51:34 -0600

    calculator

---
+++ notes_note/input.py
@@ -0,0 +1,9 @@
+# AS 2nd Format Output Notes
+
+name = "iudshfa"
+age = 975976179875290569156982198751749457023890421837509321805732183981729856719765401498715984729017
+print("{}{:,}".format(name,age))
+print("{}{:b}".format(name,age))
+print("{}{:E}".format(name,age))
+print("{}{:%}".format(name,age))
+print("{}{:.9f}".format(name,age))
---
+++ practices/calculator.py
@@ -0,0 +1,28 @@
+# AS 2nd Calculator
+
+number1 = input("gimme number")
+number2 = input("gimme another number")
+
+proofnumber1 = number1.isdigit()
+proofnumber2 = number2.isdigit()
+
+if proofnumber1 == True:
+    number1 = float(number1)
+else:
+    print("Put in an actual number you criminal D:")
+
+if proofnumber2 == True:
+    number2 = float(number1)
+else:
+    print("Put in an actual number you criminal D:")
+
+add = number1 + number2
+minus = number1 - number2
+multy = number1 * number2
+divide = number1 / number2
+modulus = number1 % number2
+
+if add == 69 or minus == 69 or multy == 69 or divide == 69 or modulus == 69:
+    print("no")
+elif add == 69 or minus == 69 or multy == 69 or divide == 69 or modulus == 69:
+    print("no")
