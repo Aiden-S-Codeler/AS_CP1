2a9bad860b235907326a3f6297723e0731efe6de
Aiden-S-Codeler <aiden.sallee@ucas-edu.net>
2025-09-09 10:12:16 -0600

    fixed fixly

--- practices/calculator.py
+++ practices/calculator.py
@@ -7,12 +7,12 @@ proofnumber1 = number1.isdigit()
 proofnumber2 = number2.isdigit()
 
 if proofnumber1 == True:
-    number1 = float(number1)
+    number1 = int(number1)
 else:
     print("Put in an actual number you criminal D:")
 
 if proofnumber2 == True:
-    number2 = float(number1)
+    number2 = int(number1)
 else:
     print("Put in an actual number you criminal D:")
 
@@ -24,5 +24,13 @@ modulus = number1 % number2
 
 if add == 69 or minus == 69 or multy == 69 or divide == 69 or modulus == 69:
     print("no")
-elif add == 69 or minus == 69 or multy == 69 or divide == 69 or modulus == 69:
+elif add == 67 or minus == 67 or multy == 67 or divide == 67 or modulus == 67:
     print("no")
+elif add == 420 or minus == 420 or multy == 420 or divide == 420 or modulus == 420:
+    print("no")
+else:
+    print(add)
+    print(minus)
+    print(multy)
+    print(divide)
+    print(modulus)
