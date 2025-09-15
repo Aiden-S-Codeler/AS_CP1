4733b254f79065d441e79872f4324b630ae0e85c
Aiden-S-Codeler <aiden.sallee@ucas-edu.net>
2025-09-15 09:56:42 -0600

    typeing stuff

---
+++ actual_work/yargh_moneysplit.py
@@ -0,0 +1,5 @@
+# as 2nd Prite Treasure Split
+
+print("You are Pirate Jim. \nYour crew got a large amount of money from a recent sale of plundered goods. \nEach member already got $500 dollars for the job.")
+bounty = int(input("how much money is there post crew payment (after everyone gets 500 dollars)"))
+crew_size = int(input("how big is the crew (not including you, the captain, and the first mate)"))
