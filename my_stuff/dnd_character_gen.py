55ccb982bcf1e86b486434727c4118bdbe730c9a
Aiden-S-Codeler <aiden.sallee@ucas-edu.net>
2025-09-17 10:50:41 -0600

    made random stat maker :)

--- my_stuff/dnd_character_gen.py
+++ my_stuff/dnd_character_gen.py
@@ -1,9 +1,14 @@
 # AS dnd random gen
 import random
-stat_1 = int(random.randint(1, 10))
-stat_2 = int(random.randint(1, 10))
-stat_3 = int(random.randint(1, 10))
-stat_4 = int(random.randint(1, 10))
-D6outcome = [num_1, num_2, num_3, num_4]
-D6outcome.remove(min(D6outcome))
-print(D6outcome)
+stat_1 = [int(random.randint(1, 6)), int(random.randint(1, 6)), int(random.randint(1, 6)), int(random.randint(1, 6))]
+stat_1.remove(min(stat_1))
+stat_2 = [int(random.randint(1, 6)), int(random.randint(1, 6)), int(random.randint(1, 6)), int(random.randint(1, 6))]
+stat_2.remove(min(stat_2))
+stat_3 = [int(random.randint(1, 6)), int(random.randint(1, 6)), int(random.randint(1, 6)), int(random.randint(1, 6))]
+stat_3.remove(min(stat_3))
+stat_4 = [int(random.randint(1, 6)), int(random.randint(1, 6)), int(random.randint(1, 6)), int(random.randint(1, 6))]
+stat_4.remove(min(stat_4))
+stat_5 = [int(random.randint(1, 6)), int(random.randint(1, 6)), int(random.randint(1, 6)), int(random.randint(1, 6))]
+stat_5.remove(min(stat_5))
+stat_6 = [int(random.randint(1, 6)), int(random.randint(1, 6)), int(random.randint(1, 6)), int(random.randint(1, 6))]
+stat_6.remove(min(stat_6))
