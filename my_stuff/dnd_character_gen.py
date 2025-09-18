cd333b0cc115ba5264aa8b574cc78d07f942dcef
Aiden-S-Codeler <aiden.sallee@ucas-edu.net>
2025-09-18 18:19:58 +0000

    added race WIP

--- my_stuff/dnd_character_gen.py
+++ my_stuff/dnd_character_gen.py
@@ -25,3 +25,11 @@ class_list = ["Artificer", "Barbarian", "Bard", "Cleric", "Druid", "Fighter", "M
 rand_class = int(random.randint(0, 12))
 class_chosen = class_list[rand_class]
 print(f"Your class is: {class_chosen}")
+
+race_list = ["Dragon Born", "Dwarf", "Elf", "Gnome", "Half Elf", "Halfling", "Half Orc", "Human", "Teifling", "Aarakocra", "Aasimar", "Air Genasi", "Bug Bear", "Centaur", "Changeling", "Deep Gnome", "Duergar", "Earth Genasi", "Eladrin", "Fairy", "Firbolg", "Fire Genasi", "Githyanki", "Githzerai", "Goblin", "Goliath", "Harengon", "Hobgoblin", "Kenku", "Kobold", "Lizardfolk", "Minotaur", "Orc", "Satyr", "Sea Elf", "Shadar Kai", "Shifter", "Tabaxi", "Tortle", "Triton", "Water Genasi", "Yuan-Ti"]
+rand_race = int(random.randint(0, 41))
+race_chosen = race_list[rand_race]
+if rand_race == 0:
+    dragon_born_list = ["Red", "Blue", "Black", "White", "Green", "Copper", "Gold", "Brass", "Silver", "Bronze", "", "", "", "", ""]
+    rand_dragon_born = int(random.randint(0, 14))
+print(f"Your race is: {race_chosen}")
