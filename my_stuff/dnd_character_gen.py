59406f65976c6e5c008db7242249a010ca41e064
Aiden-S-Codeler <aiden.sallee@ucas-edu.net>
2025-09-18 01:47:37 +0000

    made it give you the number directly

--- my_stuff/dnd_character_gen.py
+++ my_stuff/dnd_character_gen.py
@@ -2,13 +2,25 @@
 import random
 stat_1 = [int(random.randint(1, 6)), int(random.randint(1, 6)), int(random.randint(1, 6)), int(random.randint(1, 6))]
 stat_1.remove(min(stat_1))
+stat_1 = sum(stat_1)
+print(stat_1)
 stat_2 = [int(random.randint(1, 6)), int(random.randint(1, 6)), int(random.randint(1, 6)), int(random.randint(1, 6))]
 stat_2.remove(min(stat_2))
+stat_2 = sum(stat_2)
+print(stat_2)
 stat_3 = [int(random.randint(1, 6)), int(random.randint(1, 6)), int(random.randint(1, 6)), int(random.randint(1, 6))]
 stat_3.remove(min(stat_3))
+stat_3 = sum(stat_3)
+print(stat_3)
 stat_4 = [int(random.randint(1, 6)), int(random.randint(1, 6)), int(random.randint(1, 6)), int(random.randint(1, 6))]
 stat_4.remove(min(stat_4))
+stat_4 = sum(stat_4)
+print(stat_4)
 stat_5 = [int(random.randint(1, 6)), int(random.randint(1, 6)), int(random.randint(1, 6)), int(random.randint(1, 6))]
 stat_5.remove(min(stat_5))
+stat_5 = sum(stat_5)
+print(stat_5)
 stat_6 = [int(random.randint(1, 6)), int(random.randint(1, 6)), int(random.randint(1, 6)), int(random.randint(1, 6))]
 stat_6.remove(min(stat_6))
+stat_6 = sum(stat_6)
+print(stat_6)
