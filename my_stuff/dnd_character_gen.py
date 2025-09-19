29d99250a0deffc7eb71b09e685b716a32fe6a26
Aiden-S-Codeler <aiden.sallee@ucas-edu.net>
2025-09-19 02:37:14 +0000

    added background information generation

--- my_stuff/dnd_character_gen.py
+++ my_stuff/dnd_character_gen.py
@@ -1,4 +1,5 @@
 # AS dnd random gen
+# Stats
 
 import random
 stat_1 = [int(random.randint(1, 6)), int(random.randint(1, 6)), int(random.randint(1, 6)), int(random.randint(1, 6))]
@@ -21,10 +22,14 @@ stat_6.remove(min(stat_6))
 stat_6 = sum(stat_6)
 print(f"Your stats are: {stat_1}, {stat_2}, {stat_3}, {stat_4}, {stat_5}, {stat_6}")
 
+# Class
+
 class_list = ["Artificer", "Barbarian", "Bard", "Cleric", "Druid", "Fighter", "Monk", "Paladin", "Ranger", "Rogue", "Sorceror", "Warlock", "Wizard"]
 rand_class = int(random.randint(0, 12))
 class_chosen = class_list[rand_class]
 print(f"Your class is: {class_chosen}")
+
+# Race
 
 race_list = ["Dragonborn", "Dwarf", "Elf", "Gnome", "Half Elf", "Halfling", "Half Orc", "Human", "Variant Human", "Teifling", "Aarakocra", "Aasimar", "Air Genasi", "Bug Bear", "Centaur", "Changeling", "Deep Gnome", "Duergar", "Earth Genasi", "Eladrin", "Fairy", "Firbolg", "Fire Genasi", "Githyanki", "Githzerai", "Goblin", "Goliath", "Harengon", "Hobgoblin", "Kenku", "Kobold", "Lizardfolk", "Minotaur", "Orc", "Satyr", "Sea Elf", "Shadar Kai", "Shifter", "Tabaxi", "Tortle", "Triton", "Water Genasi", "Yuan-Ti"]
 rand_race = int(random.randint(0, 41))
@@ -35,3 +40,24 @@ if rand_race == 0:
 else:
     race_chosen = race_list[rand_race]
 print(f"Your race is: {race_chosen}")
+
+# Background
+
+rand_personality = random.randint(1, 8)
+rand_ideal = random.randint(1, 6)
+rand_bond = random.randint(1, 6)
+rand_flaw = random.randint(1, 6)
+print(f"Your first rolls for background information are: Personality Trait: {rand_personality}, Ideal: {rand_ideal}, Bond: {rand_bond}, Flaw: {rand_flaw}")
+rand_personality2 = random.randint(1, 8)
+rand_ideal2 = random.randint(1, 6)
+rand_bond2 = random.randint(1, 6)
+rand_flaw2 = random.randint(1, 6)
+while rand_personality2 == rand_personality:
+    rand_personality2 = random.randint(1, 8)
+while rand_ideal2 == rand_ideal:
+    rand_ideal2 = random.randint(1, 6)
+while rand_bond2 == rand_bond:
+    rand_bond2 = random.randint(1, 6)
+while rand_flaw2 == rand_flaw:
+    rand_flaw2 = random.randint(1, 6)
+print(f"Your second rolls for background information are: Personality Trait: {rand_personality2}, Ideal: {rand_ideal2}, Bond: {rand_bond2}, Flaw: {rand_flaw2}")
