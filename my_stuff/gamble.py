5b8496f4a471ccd27c1d1e0b4c13d4e0959236e0
Aiden-S-Codeler <aiden.sallee@ucas-edu.net>
2025-09-30 10:50:13 -0600

    making black jack

--- my_stuff/gamble.py
+++ my_stuff/gamble.py
@@ -3,6 +3,7 @@ start = False
 chosen = False
 balance = 100
 bet = 0
+card_list = []
 import random
 
 def roulette():
@@ -73,6 +74,45 @@ def slots():
         balance -= bet
         print(f"You lose. Current balance: {balance}")
     bet = 0
+
+def blackjack():
+    global balance
+    global bet
+    global card_list
+    start_val = random.randint(1, 10) + random.randint(1, 10)
+    deal_start_val = random.randint(1, 10) + random.randint(1, 10)
+    while deal_start_val < 16:
+        deal_start_val += random.randint(1, 10)
+        if deal_start_val > 21:
+            deal_start_val = random.randint(1, 10) + random.randint(1, 10)
+        else:
+            pass
+    is_inrange = False
+    while is_inrange == False:
+        bet = input(f"How much do you want to bet?\nCurrent balance: {balance}\n")
+        if bet.isdigit():
+            bet = int(bet)
+            if bet <= balance:
+                is_inrange = True
+            else:
+                print("Bet must be less than or equal to your balance.")
+        else:
+            print("Please choose number.")
+    done = False
+    while done == False:
+        print(f"You currently have a card value of: {start_val}")
+        hit_stand = input("Would you like to hit or stand?: ")
+        if hit_stand == "hit":
+            start_val += random.randint(1, 10)
+            if start_val > 21:
+                print(f"You busted with a value of {start_val}.")
+                balance -= bet
+                print(f"You lose. Current balance: {balance}")
+                break
+            else:
+                print(f"Current value: {start_val}")
+        else:
+            pass
 
 while start == False:
     while chosen == False:
