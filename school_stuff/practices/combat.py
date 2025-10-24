4b6ee79445fc082d1c002c299622959cb4b1e5b4
Aiden-S-Codeler <aiden.sallee@ucas-edu.net>
2025-10-24 10:51:33 -0600

    CARRY ON MY WAYWARD SOUOUOOUL

---
+++ school_stuff/practices/combat.py
@@ -0,0 +1,16 @@
+#AS 2nd combet simulator
+import character_gen_mini
+import random
+
+bear_h = random.randint(1, 10) + random.randint(1, 10) + random.randint(1, 10) + random.randint(1, 10) + 12
+bear_ac = 11
+claw = random.randint(1, 8) + 4
+bite = random.randint(1, 6) + random.randint(1, 6) + 4
+
+
+def fighter_turn():
+    pass
+def bard_turn():
+    pass
+def wizard_turn():
+    pass
