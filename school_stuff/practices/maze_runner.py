faa1c12039571f382d7438bacb2ac7160b3d6777
Aiden-S-Codeler <aiden.sallee@ucas-edu.net>
2025-11-04 10:51:07 -0700

    almost done

--- school_stuff/practices/maze_runner.py
+++ school_stuff/practices/maze_runner.py
@@ -6,21 +6,21 @@ import random
 
 # 8-24, seting up grids for deciding where walls go in the maze
 row_grid = [
-    ["c", "c", "c", "c", "c", "c"],
-    ["", "", "", "", "", ""],
-    ["", "", "", "", "", ""],
-    ["", "", "", "", "", ""],
-    ["", "", "", "", "", ""],
-    ["c", "c", "c", "c", "c", "c"]
+    [1, 1, 1, 1, 1, 1],
+    [0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0],
+    [1, 1, 1, 1, 1, 1]
 ]
 
 col_grid = [
-    ["c", "c", "c", "c", "c", "o"],
-    ["", "", "", "", "", ""],
-    ["", "", "", "", "", ""],
-    ["", "", "", "", "", ""],
-    ["", "", "", "", "", ""],
-    ["o", "c", "c", "c", "c", "c"]
+    [0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0]
 ]
 
 # 27-54, setting up function to check is the maze is solvable
@@ -53,20 +53,75 @@ def is_solvable(row_grid, col_grid):
     
     return False
 
-# 57-, looping maze production until the maze is solvable
-while is_solvable(row_grid, col_grid) == False:
-    for x in range(2, 5):
-        for a in range(6):
+# 57-79, looping maze design until the maze is solvable
+solvability = False
+while solvability == False:
+    for w in range(2, 5):
+        for x in range(6):
             open_close = random.randint(1, 2)
             if open_close == 1:
-                row_grid[x][a] = "o"
+                row_grid[w][x] = 0
             else:
-                row_grid[x][a] = "c"
-    for y in range(2, 5):
-        for b in range(6):
+                row_grid[w][x] = 1
+    for y in range(6):
+        for z in range(2, 5):
             open_close = random.randint(1, 2)
             if open_close == 1:
-                col_grid[y][b] = "o"
+                col_grid[y][z] = 0
             else:
-                col_grid[y][b] = "c"
-print("what a solvable maze")
+                col_grid[y][z] = 1
+    if is_solvable(row_grid, col_grid) == False:
+        pass
+    elif is_solvable(row_grid, col_grid) == True:
+        solvability = True
+    else:
+        continue
+    is_solvable(row_grid, col_grid)
+
+# 82-124, making turtles, setting them to correct specs, and screen to make maze on
+screen = turtle.Screen()
+ut1 = turtle.Turtle()
+ut2 = turtle.Turtle()
+ut3 = turtle.Turtle()
+ut4 = turtle.Turtle()
+ut5 = turtle.Turtle()
+ut6 = turtle.Turtle()
+ut1.pensize(2)
+ut2.pensize(2)
+ut3.pensize(2)
+ut4.pensize(2)
+ut5.pensize(2)
+ut6.pensize(2)
+ut1.teleport(0,30)
+ut2.teleport(10,30)
+ut3.teleport(20,30)
+ut4.teleport(30,30)
+ut5.teleport(40,30)
+ut6.teleport(50,30)
+ut1.right(90)
+ut2.right(90)
+ut3.right(90)
+ut4.right(90)
+ut5.right(90)
+ut6.right(90)
+st1 = turtle.Turtle()
+st2 = turtle.Turtle()
+st3 = turtle.Turtle()
+st4 = turtle.Turtle()
+st5 = turtle.Turtle()
+st6 = turtle.Turtle()
+st1.pensize(2)
+st2.pensize(2)
+st3.pensize(2)
+st4.pensize(2)
+st5.pensize(2)
+st6.pensize(2)
+st1.teleport(0,30)
+st2.teleport(0,20)
+st3.teleport(0,10)
+st4.teleport(0,0)
+st5.teleport(0,-10)
+st6.teleport(0,-20)
+
+# 127-, commanding turtles to draw their part of the maze
+for 
