cefbb5e44f2dfb5cadb0e8bf51fcc509788adabc
Aiden-S-Codeler <aiden.sallee@ucas-edu.net>
2025-11-05 10:52:26 -0700

    hgfdn

--- school_stuff/practices/maze_runner.py
+++ school_stuff/practices/maze_runner.py
@@ -3,7 +3,7 @@
 # 4-5, importing things need for mave generation
 import turtle
 import random
-
+solvability = False
 # 8-24, seting up grids for deciding where walls go in the maze
 row_grid = [
     [1, 1, 1, 1, 1, 1],
@@ -15,12 +15,12 @@ row_grid = [
 ]
 
 col_grid = [
-    [0, 0, 0, 0, 0, 1],
-    [1, 0, 0, 0, 0, 1],
-    [1, 0, 0, 0, 0, 1],
-    [1, 0, 0, 0, 0, 1],
-    [1, 0, 0, 0, 0, 1],
-    [1, 0, 0, 0, 0, 0]
+    [0, '', '', '', '', 1],
+    [1, '', '', '', '', 1],
+    [1, '', '', '', '', 1],
+    [1, '', '', '', '', 1],
+    [1, '', '', '', '', 1],
+    [1, '', '', '', '', 0]
 ]
 
 # 27-54, setting up function to check is the maze is solvable
@@ -53,57 +53,35 @@ def is_solvable(row_grid, col_grid):
     
     return False
 
-# 57-79, looping maze design until the maze is solvable
-solvability = False
-while solvability == False:
-    for w in range(2, 5):
-        for x in range(6):
-            open_close = random.randint(1, 2)
-            if open_close == 1:
+# 57-79, make random maze
+
+def turtle_engineering(row_grid, col_grid):
+    for w in [1, 2, 3, 4]:
+        for x in [0, 1, 2, 3, 4, 5]:
+            open_close = random.randint(0, 1)
+            if open_close == 0:
                 row_grid[w][x] = 0
             else:
                 row_grid[w][x] = 1
-    for y in range(6):
-        for z in range(2, 5):
-            open_close = random.randint(1, 2)
-            if open_close == 1:
-                col_grid[y][z] = 0
+    for column1 in col_grid:
+        for row2 in column1:
+            if row2 == 1 or row2 == 0:
+                pass
             else:
-                col_grid[y][z] = 1
-    if is_solvable(row_grid, col_grid) == False:
-        pass
-    elif is_solvable(row_grid, col_grid) == True:
-        solvability = True
-    else:
-        continue
-    is_solvable(row_grid, col_grid)
+                open_close = random.randint(0, 1)
+                if open_close == 0:
+                    row_grid[w][x] = 0
+                else:
+                    row_grid[w][x] = 1
+    return(row_grid, col_grid)
+row_grid, col_grid = turtle_engineering(row_grid, col_grid)
 
 # 82-, making turtles, setting them to correct specs, and screen to make maze on
 screen = turtle.Screen()
 ut1 = turtle.Turtle()
-ut2 = turtle.Turtle()
-ut3 = turtle.Turtle()
-ut4 = turtle.Turtle()
-ut5 = turtle.Turtle()
-ut6 = turtle.Turtle()
 ut1.pensize(2)
-ut2.pensize(2)
-ut3.pensize(2)
-ut4.pensize(2)
-ut5.pensize(2)
-ut6.pensize(2)
 ut1.teleport(0,30)
-ut2.teleport(10,30)
-ut3.teleport(20,30)
-ut4.teleport(30,30)
-ut5.teleport(40,30)
-ut6.teleport(50,30)
 ut1.right(90)
-ut2.right(90)
-ut3.right(90)
-ut4.right(90)
-ut5.right(90)
-ut6.right(90)
 st1 = turtle.Turtle()
 st2 = turtle.Turtle()
 st3 = turtle.Turtle()
@@ -117,19 +95,24 @@ st4.pensize(2)
 st5.pensize(2)
 st6.pensize(2)
 st1.teleport(0,30)
-st2.teleport(0,20)
-st3.teleport(0,10)
-st4.teleport(0,0)
-st5.teleport(0,-10)
-st6.teleport(0,-20)
+st2.teleport(0,10)
+st3.teleport(0,-10)
+st4.teleport(0,-30)
+st5.teleport(0,-50)
+st6.teleport(0,-70)
 
-# 127-, commanding turtles to draw their part of the maze
-for a in range(6):
-    if col_grid[1][a] == 1:
-        ut1.pendown
-        ut1.forward(10)
-    else:
-        ut1.penup
-        ut1.forward(10)
+# -, commanding turtles to draw their part of the maze
+xval = 0
+for column in col_grid:
+    for row in column:
+        if row == 1:
+            ut1.pendown()
+            ut1.forward(20)
+        else:
+            ut1.penup()
+            ut1.forward(20)
+    xval += 20
+    ut1.teleport(xval, 30)
+    
 
-turtle.done
+turtle.done()
