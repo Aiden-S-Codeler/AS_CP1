7f2bf25fbbfc6d2042c14dea0fc6aad772e3056f
Aiden-S-Codeler <aiden.sallee@ucas-edu.net>
2025-12-08 10:48:53 -0700

    what a fun game

--- my_stuff/choose_your_own_adventure.py
+++
@@ -1,2 +0,0 @@
-import dnd_character_gen
-
--- school_stuff/OnlyForward.py
+++ school_stuff/OnlyForward.py
@@ -3,7 +3,7 @@
 import random
 
 print("placeholder")
-print(input("press enter to start"))
+print(input("press enter to start "))
 
 money = 0
 
@@ -21,9 +21,9 @@ has_schostick = False
 
 has_sax = False
 has_greenam = False
-has_bomb = False
-has_wararmor = False
-has_warstick = False
+has_drums = False
+has_piccolo = False
+has_sunglasses = False
 
 stat_1 = [int(random.randint(1, 6)), int(random.randint(1, 6)), int(random.randint(1, 6)), int(random.randint(1, 6))]
 stat_1.remove(min(stat_1))
@@ -40,3 +40,116 @@ stat_4 = sum(stat_4)
 
 stats = [stat_1, stat_2, stat_3, stat_4]
 
+while True:
+    class_choose = input("What class would you like (warrior, scholar, or bard)? ")
+    if class_choose == "warrior":
+        uclass = "warrior"
+        health = 60
+        ac = 17
+    elif class_choose == "scholar":
+        uclass = "scholar"
+        health = 40
+        ac = 13
+    elif class_choose == "bard":
+        uclass = "bard"
+        health = 50
+        ac = 15
+    else:
+        continue
+    break
+
+strength = 0
+constitution = 0
+inteligence = 0
+charisma = 0
+
+print(stats)
+while True:
+    while strength == 0:
+        what_stat = input("Which of your stats do you want to be your strength? ")
+        if what_stat.isdigit():
+            what_stat = int(what_stat)
+        else:
+            print("That is not a digit.")
+            continue
+        if what_stat in stats:
+            strength = what_stat
+            stats.remove(what_stat)
+        else:
+            print("That number is not available.")
+            continue
+    print(f"remaining stats are: {stats}")
+    while constitution == 0:
+        what_stat = input("Which of your stats do you want to be your constitution? ")
+        if what_stat.isdigit():
+            what_stat = int(what_stat)
+        else:
+            print("That is not a digit.")
+            continue
+        if what_stat in stats:
+            constitution = what_stat
+            stats.remove(what_stat)
+        else:
+            print("That number is not available.")
+            continue
+    print(f"remaining stats are: {stats}")
+    while inteligence == 0:
+        what_stat = input("Which of your stats do you want to be your inteligence? ")
+        if what_stat.isdigit():
+            what_stat = int(what_stat)
+        else:
+            print("That is not a digit.")
+            continue
+        if what_stat in stats:
+            inteligence = what_stat
+            stats.remove(what_stat)
+        else:
+            print("That number is not available.")
+            continue
+    print(f"remaining stats are: {stats}")
+    while charisma == 0:
+        what_stat = input("Which of your stats do you want to be your charisma? ")
+        if what_stat.isdigit():
+            what_stat = int(what_stat)
+        else:
+            print("That is not a digit.")
+            continue
+        if what_stat in stats:
+            charisma = what_stat
+            stats.remove(what_stat)
+        else:
+            print("That number is not available.")
+            continue
+    break
+
+def firepath(uclass, money, strength, constitution, inteligence, charisma, has_coolsword, has_redam, has_bomb, has_wararmor, has_warstick, has_bhstaff, has_blueam, has_book, has_schocloak, has_schostick, has_sax, has_greenam, has_drum, has_piccolo, has_sunglasses):
+    print(input("You chose the fire path.\nThe moment you enter, you feel the heat of the magma flowing down the cave walls radiating onto you.\nYou also see three chests. "))
+    visitchest1 = False
+    visitchest3 = False
+    while True:
+        whatchest = input("What chest do you choose: 1, 2, or 3? ")
+        if whatchest == "1" or whatchest == "2" or whatchest == "3":
+            if whatchest == "1":
+                if visitchest1 == False:
+                    has_coolsword = True
+                    print(input("Congrats! You have picked up the Really Cool Sword! "))
+                    visitchest1 = True
+                else:
+                    print("You have already checked here.")
+            elif whatchest == "2":
+                print(input("KABOOM!!! The chest explodes, destroying the other two chests in the process.\n You leave for the next area. "))
+                break
+            elif whatchest == "3":
+                if visitchest3 == False:
+                    has_coolsword = True
+                    print(input("Congrats! You have picked up $10! "))
+                    visitchest3 = True
+                else:
+                    print("You have already checked here.")
+        else:
+            print("Invalid choice.")
+            continue
+    
+    print(input("You enter the next cavern and here a skuttering sound from above you. As you look up, you see a green creature leap down from the ceiling at you.\nAs you leap back to dodge, you can tell the the creature is a goblingo. "))
+    while True:
+        pass
